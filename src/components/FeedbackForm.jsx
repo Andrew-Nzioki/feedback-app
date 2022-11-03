701d90e9fdd29513d7d1dcb5e0498c42a7957ae4
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2022-11-03 05:57:42 +0300

    added the some logic to how the text input is handled

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,22 @@
 import { useState } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters');
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
@@ -19,10 +32,11 @@ function FeedbackForm() {
             placeholder="Write your Review"
             value={text}
           />
-          <Button type="submit" version="secondary">
+          <Button type="submit" isDisabled={btnDisabled} disabled>
             Send
           </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
