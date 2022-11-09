d1aad892c94490109aa20df3967f23ebba46a823
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2022-11-09 21:56:14 +0300

    added state to about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import { useState } from 'react';
 function AboutPage() {
-  return <div>AboutPage</div>;
+  const [mew, setMew] = useState(10);
+  const text = () => setMew(mew * 2);
+  return (
+    <>
+      <p>{mew}</p>
+      <button onClick={text}>Click me</button>
+    </>
+  );
 }
 
 export default AboutPage;
